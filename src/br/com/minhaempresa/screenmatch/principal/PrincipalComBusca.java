ea74a094ec06235332b4e2b109fdc5bd1ddfde55
mateus <mateus.bevenutti@gmail.com>
2026-06-30 18:37:37 -0300

    Adiciona try-catch para tratar excessão de conversão de numero

--- src/br/com/minhaempresa/screenmatch/principal/PrincipalComBusca.java
+++ src/br/com/minhaempresa/screenmatch/principal/PrincipalComBusca.java
@@ -1,5 +1,11 @@
 package br.com.minhaempresa.screenmatch.principal;
 
+import br.com.minhaempresa.screenmatch.modelos.Titulo;
+import br.com.minhaempresa.screenmatch.modelos.TituloOmdb;
+import com.google.gson.FieldNamingPolicy;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
@@ -21,6 +27,25 @@ public class PrincipalComBusca {
                 .build();
         HttpResponse<String> response = client
                 .send(request, HttpResponse.BodyHandlers.ofString());
-        System.out.println(response.body());
+
+        String json = response.body();
+        System.out.println(json);
+
+
+        Gson gson = new GsonBuilder()
+                .setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE).create();
+
+        TituloOmdb meuTituloOmdb = gson.fromJson(json, TituloOmdb.class);
+        System.out.println(meuTituloOmdb);
+        try {
+            Titulo meuTitulo = new Titulo(meuTituloOmdb);
+            System.out.println("Titulo já convertido");
+            System.out.println(meuTitulo);
+        } catch (NumberFormatException e){
+            System.out.println("Aconteceu um erro: " + e.getMessage());
+        } finally {
+            System.out.println("Finalizado com sucesso");
+        }
+
     }
 }
